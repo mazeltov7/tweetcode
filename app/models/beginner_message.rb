b4f62ea0512c2795c8ea3dc8e592e54675eecdaf
mazeltov7 <ishikawayuki2@gmail.com>
2013-09-13 18:57:16 +0900

    beginner_messageモデル作成、migrateだん

---
+++ app/models/beginner_message.rb
@@ -0,0 +1,2 @@
+class BeginnerMessage < ActiveRecord::Base
+end
---
+++ test/models/beginner_message_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BeginnerMessageTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
