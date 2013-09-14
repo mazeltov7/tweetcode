0570bb425fe868e54012bc268da8fec61d1e5faa
mazeltov7 <ishikawayuki2@gmail.com>
2013-09-14 19:14:37 +0900

    usernameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130914081452) do
+ActiveRecord::Schema.define(version: 20130914101246) do
 
   create_table "beginner_messages", force: true do |t|
     t.text     "body"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20130914081452) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
