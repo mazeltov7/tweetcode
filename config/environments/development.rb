5d1c2e1006502f2a450124949b83cd508f6e6a3a
mazeltov7 <ishikawayuki2@gmail.com>
2013-09-16 17:28:04 +0900

    smtpは後ほど

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,18 @@ Tweetcode::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #:address => 'smtp.gmail.com',
+  #:port => 587,
+  #:authentication => :plain,
+  #:user_name => 'mazeltov17@gmail.com',
+  #:password => 'funbestothers7'
+  #}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -27,6 +38,5 @@ Tweetcode::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 end
