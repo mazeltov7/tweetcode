978fcf01eabab9c94965696a57a7daf721642204
mazeltov7 <ishikawayuki2@gmail.com>
2013-09-19 17:25:50 +0900

    railscast ActionController::Liveをやってみた。が非同期通信がまだうまくいっていない。

--- app/assets/javascripts/beginner_messages.js.coffee
+++ app/assets/javascripts/beginner_messages.js.coffee
@@ -1,3 +1,8 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+source = new EventSource('/beginner_messages/events')
+source.addEventListener 'beginner_messages.create', (e) ->
+  beginner_message = $.parseJSON(e.data).beginner_message
+  $('#chat').append($('<li>').text("#{beginner_message.body}"))
--- app/controllers/beginner_messages_controller.rb
+++ app/controllers/beginner_messages_controller.rb
@@ -8,11 +8,26 @@ class BeginnerMessagesController < ApplicationController
   end
 
   def create
+    response.headers["Content-Type"] = "text/javascript"
     attributes = params.require(:beginner_message).permit(:body)
-    @message = BeginnerMessage.create!(attributes)
+    @message = BeginnerMessage.create(attributes)
+    $redis.publish('beginner_messages.create', @message.to_json)
   end
 
-  def event
+  def events
+    response.headers["Content-Type"] = "text/event-stream"
+    redis = Redis.new
+    redis.psubscribe('beginner_messages.*') do |on|
+      on.pbeginner_message do |pattern, event, data|
+        response.stream.write("event: #{event}|n")
+        response.stream.write("data: #{data}|n|n")
+      end
+    end
+  rescue IOError
+    logger.info "Stream closed"
+  ensure
+    redis.quit
+    response.stream.close
   end
   
 end
