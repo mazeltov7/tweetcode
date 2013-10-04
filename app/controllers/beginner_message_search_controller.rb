4cca358d3899cb1094ef7468050fc6c3d5010f11
mazeltov7 <ishikawayuki2@gmail.com>
2013-10-05 03:58:59 +0900

    検索一応対応、日本語対応できておらず

--- app/controllers/beginner_message_search_controller.rb
+++ app/controllers/beginner_message_search_controller.rb
@@ -1,2 +1,7 @@
+# coding:utf-8
+
 class BeginnerMessageSearchController < ApplicationController
+  def index
+    @beginner_message_saerches = BeginnerMessage.text_search(params[:query])
+  end 
 end
--- app/models/beginner_message.rb
+++ app/models/beginner_message.rb
@@ -1,3 +1,16 @@
+# coding:utf-8
+
 class BeginnerMessage < ActiveRecord::Base
   belongs_to :user
+
+  def self.text_search(query)
+    if query.present?
+      rank =  <<-RANK
+        ts_rank(to_tsvector(body),plainto_tsquery(#{sanitize(query)}))
+      RANK
+      where("body @@ :q ", q: query).order("#{rank} desc")
+    else
+      scope
+    end
+  end
 end
