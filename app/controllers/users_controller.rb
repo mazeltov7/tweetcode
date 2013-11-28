7478365eecd8e4661c5f2f0309f0e5121c4aff5c
mazeltov7 <ishikawayuki2@gmail.com>
2013-11-29 00:55:51 +0900

    とりあえずgood

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,32 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_url, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end    
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :status, :profile)
   end
 
 end
