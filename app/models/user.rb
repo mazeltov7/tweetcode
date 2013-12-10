cc3bca8c590f0b47b477ae336d587ce7ce478189
mazeltov7 <ishikawayuki2@gmail.com>
2013-12-10 20:17:18 +0900

    status, usernameにvalidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_many :beginner_messages
-  validates :username, presence: true
-  validates :status, presence: true
+  validates :username,
+    presence: true,
+    length: {minimum: 1, maximum: 15}
+  validates :status,
+    presence: true,
+    length: {minimum: 1, maximum: 15}
 
   def self.create_with_omniauth(auth)
     create! do |user|
