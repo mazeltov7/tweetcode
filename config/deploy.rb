056cc6df1cedef11567a891b6780dc546a311fb7
mazeltov7 <ishikawayuki2@gmail.com>
2013-12-25 03:50:32 +0900

    しゅせい

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :branch, 'master'
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.0.0-p247'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_custom_path, '/usr/local/rbenv'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_custom_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_custom_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
