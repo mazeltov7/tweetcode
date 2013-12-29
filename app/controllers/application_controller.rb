6bd3c49e2c78bf94886f6c25b07ba682330f8f06
mazeltov7 <ishikawayuki2@gmail.com>
2013-12-30 02:31:26 +0900

    やはり    rescue ActiveRecord::RecordNotFound 追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
 
 
